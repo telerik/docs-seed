63a53dea8ea578a849a915596801215da0f80bd4
imtodor <tmitev89@gmail.com>
2018-06-06 11:10:15 +0300

    fix: Build steps for cloning repository and building site added

--- build.sh
+++ build.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 
-# Use that file in documentation builds 
+repository=$1
+branch=$2
+clone_path=".tmp/"
 
-function copy {
-  echo "copying ..."
-  rsync -r $1 $2
-}
+rm -rf $clone_path
+mkdir $clone_path
 
-# The repo for which we would build the site has been already pulled out...
-# TODO: Guard check for all the parameters...
+git clone -b $branch $repository $clone_path
 
-# Here we should copy from .tmp to the corresponding directories. 
-# From the config file, we would get the controls/libraries and the rest of the docs (installation, FAQ, etc) 
-# to place in the 'components' section of the site.
-
-ROOT_PATH=".tmp/"$1
-echo "copying from "$ROOT_PATH
-
-CONTROLS_PATH=$2
-# echo "control are in '"CONTROLS_PATH"' folder'"
-
-# TODO: Copy the files/directories including assets and _config to the necessary place 
+cp -r $clone_path/* "./"
 
 echo "Start building..."
-bundle exec jekyll $3 --config _config.yml
+bundle exec jekyll build --config _config.yml
