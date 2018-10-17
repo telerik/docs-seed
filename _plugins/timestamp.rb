9b2895605635cfcde47821e4adf2732309258e94
imtodor <tmitev89@gmail.com>
2018-10-17 10:15:11 +0300

    chore: Sitemap generation removed for faster build

--- _plugins/timestamp.rb
+++ _plugins/timestamp.rb
@@ -8,7 +8,8 @@ class TimeStampTag < Liquid::Block
         contents = super
         content = Liquid::Template.parse(contents).render context
 		# aim for YYYY-MM-DD format https://www.google.com/sitemaps/protocol.html#lastmoddef
-       `git log -1 --format=%cd --date=short #{content}`.strip
+       #`git log -1 --format=%cd --date=short #{content}`.strip
+       "2018-10-17"
     end
 end
 
