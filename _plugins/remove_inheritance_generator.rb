81b717cf496bfc968b4ff730cc369ae60ab44c10
imtodor <tmitev89@gmail.com>
2018-10-25 17:42:29 +0300

    chore: Plugin scoped to ajax-docs only

--- _plugins/remove_inheritance_generator.rb
+++ _plugins/remove_inheritance_generator.rb
@@ -1,12 +1,13 @@
 module Reading
   class RemoveInheritanceGenerator < Jekyll::Generator
-		def generate(site)	
-			@site = site
-			site.pages.each do |p|			
-				if p.data["description"] == "Client-side API Reference"
-					p.content.sub!(/(^#[^:]*).*/, '\1')
-				end
-			end
+    def generate(site)	
+      if site.platform == "aspnet-ajax"
+        site.pages.each do |p|			
+          if p.data["description"] == "Client-side API Reference"
+            p.content.sub!(/(^#[^:]*).*/, '\1')
+          end
+        end
+      end
 		end
 	end
 end
