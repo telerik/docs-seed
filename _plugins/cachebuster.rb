9c4d1049564c3dae6704390f4849a61c94bd5d02
imtodor <tmitev89@gmail.com>
2020-06-03 10:41:30 +0300

    feat: Load js async and workaround cachebusting

--- _plugins/cachebuster.rb
+++ _plugins/cachebuster.rb
@@ -5,3 +5,8 @@ Jekyll::Hooks.register :site, :after_init do |site, payload|
     site.config['assets']['compress'] = { 'js' => true, 'css' => true}
   end
 end
+
+Jekyll::Hooks.register :site, :post_write do |site|
+  path_to_file = "#{site.config['destination']}/to_delete.html"
+  File.delete(path_to_file) if File.exist?(path_to_file)
+end
