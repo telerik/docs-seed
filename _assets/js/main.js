7c8fe15974090095293e249a89ae623087d9f75c
imtodor <tmitev89@gmail.com>
2020-06-11 11:27:43 +0300

    chore: Load the whole JS in one place and add navigation and GTM

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,14 +1,25 @@
+//= require jquery
+
 //= require bootstrap.min
 //= require prettify
 //= require clipboard.min
+
+//= require kendo/kendo.core.min
+//= require kendo/kendo.data.min
+//= require kendo/kendo.binder.min
+//= require kendo/kendo.popup.min
+//= require kendo/kendo.pager.min
+//= require kendo/kendo.listview.min
+//= require kendo/kendo.treeview.min
 
 //= require tracking
 //= require top-menu
 //= require related-articles
 //= require toc
 //= require code-snippet
-//= require sdk-browser
 //= require side-nav
 //= require additional-content
 //= require search-results
 //= require handlers
+
+//= require feedback
