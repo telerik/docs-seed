53669cdb997dad932b38add95c200db6e808db6e
Ianko Djemerenov <iankodj@gmail.com>
2021-05-13 11:10:33 +0300

    feat: add centralized announcements #149

--- _assets/js/announcement.js
+++ _assets/js/announcement.js
@@ -1,3 +1,57 @@
 $(document).ready(function () {
-    $('.js-r1dt').insertBefore($('#js-tlrk-nav')).end().show();
+    var banner = $("#announcement-banner");
+    var fallbackContent = $("#announcement-fallback").html();
+    var content = $("#announcement-content");
+    var button = $("#announcement-banner .close-button-container");
+    var url = content.data("url");
+    var storageKey = "T_DOCUMENTATION_announcement".toUpperCase();
+
+    // detect any user interaction
+    $("body").one("mousemove scroll touchstart", onUserInteraction);
+
+    function onUserInteraction () {
+        fetchAnnouncement();
+    }
+
+    function fetchAnnouncement () {
+        $.get(url).done(onSuccess).fail(onFail);
+    }
+
+    function onSuccess (data) {
+        tryRenderAnnouncement(data);
+    }
+
+    function onFail () {
+        if (fallbackContent) {
+            tryRenderAnnouncement(fallbackContent);
+        }
+    }
+
+    function tryRenderAnnouncement (html) {
+        if (!checkStorage(hash($(html).text()))) {
+            content.append(html);
+            banner.show();
+            button.one("click", hideAnnouncement);
+        }
+    }
+
+    function hideAnnouncement () {
+        saveStorage(hash(content.text()));
+        content.html("");
+        banner.hide();
+    }
+
+    function checkStorage (hash) {
+        var item = localStorage.getItem(storageKey);
+
+        return hash === item;
+    }
+
+    function saveStorage (hash) {
+        localStorage.setItem(storageKey, hash);
+    }
+
+    function hash (content) {return  "announcement-" + content.trim().split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0) };
 });
+
+
