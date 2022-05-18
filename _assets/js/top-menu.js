28bdb94a6aaa02f6699abd603fc23359eb36af65
Dimo Dimov <961014+dimodi@users.noreply.github.com>
2022-05-18 18:40:34 +0300

    fix: incorrect scrollspy offset with new design

--- _assets/js/top-menu.js
+++ _assets/js/top-menu.js
@@ -1,8 +1,7 @@
-var HEADER_HEIGHT = 81;
-var TELERIKBAR_HEIGHT = 70;
-var NAVBAR_HEIGHT = 76;
+var TELERIKBAR_HEIGHT = 60;
+var SEARCHBAR_HEIGHT = 88;
 var BREADCRUMBS_HEIGHT = 20;
 var API_SCROLL_FIX = 40;
-var SCROLLSPY_OFFSET = TELERIKBAR_HEIGHT + 10; // 10 compensates for the space above the anchored heading
+var SCROLLSPY_OFFSET = TELERIKBAR_HEIGHT + SEARCHBAR_HEIGHT + 25; // 25px is for some space above the heading
 var FOOTER_DISTANCE = 20;
 var windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
