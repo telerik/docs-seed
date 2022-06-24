b6a7ae90baa0434699eee3388ed8dc6a3323c025
Peter Milchev <peter.milchev@progress.com>
2022-06-24 16:19:33 +0300

    chore: fix Style Guide message formatting

--- _plugins/generic_hooks.rb
+++ _plugins/generic_hooks.rb
@@ -1,12 +1,13 @@
 require 'jekyll-assets'
 
 Jekyll::Hooks.register :site, :after_reset do |site, payload|
-    Jekyll.logger.info STYLE_GUIDE_TEXT.green
+    Jekyll.logger.info STYLE_GUIDE_FORMATTED_TEXT.green
 end
 
-
-STYLE_GUIDE_TEXT = "\n" + "=" * 107  + "\n"\
-"|" + " " * 105  + "|\n"\
-"| Need help with the docs? Go to the Style Guide (https://docs.telerik.com/style-guide/introduction). |"\
-"\n|" + " " * 105  + "|"\
-"\n" + "=" * 107  + "\n"
+STYLE_GUIDE_TEXT = "Need help with the docs? Go to the Style Guide (https://docs.telerik.com/style-guide/introduction)."
+STYLE_GUIDE_PLACEHOLDER_LENGTH = STYLE_GUIDE_TEXT.length + 2
+STYLE_GUIDE_FORMATTED_TEXT = "\n" + "=" * (STYLE_GUIDE_PLACEHOLDER_LENGTH + 2) + "\n"\
+"|" + " " * STYLE_GUIDE_PLACEHOLDER_LENGTH + "|\n"\
+"| " + STYLE_GUIDE_TEXT + " |"\
+"\n|" + " " * STYLE_GUIDE_PLACEHOLDER_LENGTH  + "|"\
+"\n" + "=" * (STYLE_GUIDE_PLACEHOLDER_LENGTH + 2)  + "\n"
