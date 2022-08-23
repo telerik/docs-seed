7d03d80eff5a2748cccc419f73f259087e889872
Yanko Dzhemerenov <idjemere@progress.com>
2022-08-23 12:44:32 +0300

    fix: announcement banner position

--- _assets/js/side-nav.js
+++ _assets/js/side-nav.js
@@ -87,10 +87,14 @@ function setSideNavPosition() {
     var $window = $(window);
     var windowHeight = $window.height();
     var scrollFold = $window.scrollTop() + windowHeight;
-    var topNavigationHeight = ($('.SiteRibbon').outerHeight() || 0) + ($('nav.TK-Nav').height() || ($('.PRGS-Nav').height() + $('.PRGS-Bar').height()));
+    var topNavigationHeight = ($('.SiteRibbon').outerHeight() || 0) 
+        + ($('nav.TK-Nav').height() || ($('.PRGS-Nav').height() + $('.PRGS-Bar').height())) 
+        + (parseInt($('nav.TK-Nav').css("top"),10) || 0);
+    
     var progressBarHeight = $('aside.TK-Hat').height() || $('.PRGS-Bar').height();
+    
+    var top = window.scrollY > 0 ? topNavigationHeight - (progressBarHeight || 0) : topNavigationHeight;
 
-    var top = window.scrollY > 0 ? topNavigationHeight - progressBarHeight : topNavigationHeight;
     if (window.screen.availWidth < 768) {
         if (window.scrollY > 0) {
             top = $('#navbar').outerHeight();
