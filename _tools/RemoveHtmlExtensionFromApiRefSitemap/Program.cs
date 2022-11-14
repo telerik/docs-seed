b07747d50e0c77e2fdfbc29cb96ebd499f8b3f4a
Peter Milchev <peter.milchev@progress.com>
2022-11-14 16:38:46 +0200

    fix(tools): make RemoveHtmlExtensionFromApiRefSitemap NET7 compatible

--- _tools/RemoveHtmlExtensionFromApiRefSitemap/Program.cs
+++ _tools/RemoveHtmlExtensionFromApiRefSitemap/Program.cs
@@ -8,15 +8,20 @@ namespace RemoveHtmlExtensionFromApiRefSitemap
     {
         static void Main(string[] args)
         {
-            if(args.Length < 2)
+            string[] arguments = Environment.GetCommandLineArgs();
+            
+            if(arguments.Length < 2)
             {
                 throw new ArgumentNullException("You must provide a path to the drop folder of the API reference so we can alter the sitemap.xml file in it.");
             }
-            string pathToApiRefSitemap = Path.Combine(args[0], "sitemap.xml");
+                        
+            string pathToApiRefSitemap = Path.Combine(arguments[1], "sitemap.xml");
+            
             if (!File.Exists(pathToApiRefSitemap))
             {
                 throw new FileNotFoundException("API Ref sitemap file not found in the target folder");
             }
+            
             string sitemapText = File.ReadAllText(pathToApiRefSitemap);
             sitemapText = sitemapText.Replace(".html</loc>", "</loc>");
             File.WriteAllText(pathToApiRefSitemap, sitemapText);
