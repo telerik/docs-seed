870cde62266ce281eab0c04563fe3b2c944cf514
Nikolay Nenkov <nikolay.nenkov@progress.com>
2023-02-08 16:21:07 +0200

    fixed multiple dots before html extension

--- _plugins/url_downcase.rb
+++ _plugins/url_downcase.rb
@@ -3,7 +3,7 @@ module Jekyll
       alias_method :old_url, :url
     
       def url
-        old_url.downcase.gsub(/\.html(?![^?#])/, '')
+        old_url.downcase.gsub(/\.+html(?![^?#])/, '')
       end
     end
 end
